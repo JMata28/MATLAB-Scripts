61412d19d404b2ab45bab05c542ba4cf288be2ba
JMata28 <57267632+JMata28@users.noreply.github.com>
2019-11-27 16:02:28 -0500

    Comments are finished.

--- JoseCode3_3.m
+++ JoseCode3_3.m
@@ -1,26 +1,24 @@
-%The purpose of the following script of code simulates the plot of the 
-%Fourier Series coefficients Cn of the function f(x) in slide 19. 
-%After some derivation, the equation for Cn is Cn=(Tp/T)sinc((n*Tp)/T).
+%The purpose of the following script of code is to simulate the 
+%Fourier Transform of the function f(x) in slide 19. 
+%After some derivation, the equation for Cn was found to be Cn=(Tp/T)sinc((n*Tp)/T).
+%The Fourier Transform is the multiplicxation of Cn times the period T. 
 
-T = 40;        %Define the period T of the function
-Tp= T/4;      %Define the width of the "box" in the function
-              %As seen in the graph of x(t), the width is a fourth of the period
-W = (2*pi)/T; %Define the frequency of the function
-k = -20:20;     %Define the vector from one to fifty
-Wk = W.*k;     %Define the x axis for the sinc graph
-h = -20:0.001:20; %Define a vector like k but with many other points so that the plot 
-                  %is continuous 
-Wh = W.*h;        %Define the x axis for the Fourier Transform
+T = 4*pi;               %Define the period T of the function
+Tp= pi;                 %Define the width of the "box" in the function to be pi
+W0 = (2*pi)/T;          %Define the frequency W0 of the function
+k = -5*pi:W0:5*pi;      %Define the vector from -5pi to 5pi
+h = -5*pi:0.001:5*pi;   %Define a vector with the range of k but with many other points so that the plot 
+                        %is continuous.
 
-FT = (Tp/T)*sinc((h*Tp)/T);   %Calculate the Fourier Transform of the function 
-Cn = (Tp/T)*sinc((k*Tp)/T);   %Calculate the Fourier Series Coefficients
+FT = (Tp)*sinc((h*Tp)/T);   %Calculate the Fourier Transform of the function with thousands of points
+Cn = (Tp)*sinc((k*Tp)/T);   %Calculate the Fourier Series Coefficients with a lower number of points that depend on Wo
 
+%Plots both lines in the graph. 
 figure(1)
 hold on;
-plot(Wh, FT);
-stem(Wk, Cn);
+plot(h, FT);
+stem(k, Cn);
 title('Fourier Series Coefficients of the Function f(x)');
 xlabel('W*k');
 ylabel('Cn');
-%ylim([-0.1 0.3]);
 hold off; 
